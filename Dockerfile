efa2c2ca20a0913bb46adfe33f7b5f58b286252d
Hleb Rubanau <g.rubanau@gmail.com>
2020-03-09 14:12:45 +0300

    prototype

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3-alpine
+MAINTAINER Hleb Rubanau <contact@rubanau.com>
+
+ADD requirements.txt /requirements.txt
+RUN pip3 install /requirements.txt
+
+EXPOSE 8000
+WORKDIR /app
+RUN touch /app/__init__.py 
+ENV GUNICORN_CMD_ARGS="--bind=0.0.0.0:8000 --workers=3 --access-logfile - --error-logfile - --log-level debug" PYTHONUNBUFFERED=1
+ENTRYPOINT ["gunicorn"]
+CMD ["auth:app"]
+COPY auth.py /app/
+
+
---
+++ auth.py
@@ -0,0 +1,85 @@
+from flask import Flask, jsonify, request, render_template
+from wsid.basic.identification import PasswordAuthenticator, get_public_ssh_keys
+from wsid.basic.simple_policy import simple_ruleset
+import configparser
+import os.path
+
+app = Flask(__name__)
+
+WHITELIST_CONFIG_PATH='/settings/identification_whitelist.conf'
+SSH_CONFIG_PATH='/settings/ssh.conf'
+
+
+class Authenticator:
+    def __init__(self):
+        with open(WHITELIST_CONFIG_PATH,'r') as f:
+            identification_whitelist=simple_ruleset( f.readlines() )
+
+        self.authenticator=PasswordAuthenticator(whitelist=identification_whitelist)
+
+
+    def authenticate_by_password(self, user, password):
+        return self.authenticator.authenticate(user, password)
+
+class PublicSSHKeysManager:
+    def __init__(self):
+        self.ssh_config={}
+        if os.path.exists(SSH_CONFIG_PATH):
+            with open(SSH_CONFIG_PATH, 'r') as sshconf:
+                parser = configparser.ConfigParser()
+                parser.read(SSH_CONFIG_PATH)
+
+                for section in parser:
+                    username=section.split(':')[0]
+                    command = ':'.join(section.split(':')[1:]) if ':' in section else '*'
+
+                    if not username in ssh_config:
+                        self.ssh_config[username]={}
+                    if not command in self.ssh_config[username]:
+                        self.ssh_config[username][command]=set()
+
+                    for remote_identity in section:
+                        self.ssh_config[username][command].add(remote_identity)
+
+    def get_authorized_keys(self, username):
+        if not username in self.ssh_config:
+            return []
+
+        authorized_keys=[]
+        for command, identities in self.ssh_config[username].items():
+            for remote_identity in identities:
+                for k in get_public_ssh_keys(remote_identities): 
+                    if command=='*':
+                        authorized_keys.append(k)
+                    else:
+                        authorized_keys.append(f"command=\"{command}\" {k}")
+        return authorized_keys
+                         
+
+AUTHENTICATOR=Authenticator()
+PUBLIC_KEYS=PublicSSHKeysManager()
+
+# default endpoint is used to process nginx authorization requests
+@app.route("/", methods=["GET"])
+def nginx_auth():
+    # we only do validation against whitelist
+    # more sophisticated analysis (authorization against particular methods etc.) is possible but it's not implemented here
+    
+    # we expect basic authentication to contain username(WSID identity) and password 
+    if not request.authorization:
+        return '{"msg":"Authenticate!"}', 401
+
+    # we are not blindly checking each and every username
+    if not IDENTIFIER.authenticate_by_password(request.authorization.username, 
+                                               request.authorization.password):
+        return '{}', 403
+
+    # TODO: add configurable authorization policies based on confirmed identity and request parameters
+    response=jsonify({"status": "ok", "user": request.authorization.username})
+    response.headers['X-WSID-Identity']=request.authorization.username
+    return response
+
+
+@app.route("/.ssh/authorized_keys/<username>", methods=["GET"])
+def ssh_authorized_keys(username):
+    return "\n".join( PUBLIC_KEYS.get_authorized_keys(username) ) 
